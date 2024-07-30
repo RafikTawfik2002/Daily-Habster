29e2217332330814790998fca9ebf5a65dd63658
Rafik Tawfik <rafiktawfik2002@gmail.com>
2024-07-30 02:39:17 -0400

    frontend initialized

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <h1 className="text-5xl font-bold underline text-slate-400">
+      Hello world!
+    </h1>
+  )
+}
+
+export default App
